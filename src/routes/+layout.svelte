304f158d00ba1f67b7e6fe21952ef66167a0f508
RobinPH <robindimasin@gmail.com>
2024-06-29 20:55:06 +0800

    Updated Meta Tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,16 @@
 	<meta property="image" content="https://robindimasin.com/images/thumbnail.png" />
 	<meta
 		property="description"
-		content="Robin Dimasin is a Freelancer specializing in Python and Fullstack Development, offering services like Web Scraping, Browser Automation, and Task Automation. With experience as a System Analyst."
+		content="Robin Dimasin is a Freelancer specializing in Python and Fullstack Development, offering services like Web Scraping, Fullstack Development, Browser Automation using Selenium/Puppeteer, Python Scripting, Task Automation, and Chrome Extension Development. With experience as a System Analyst."
 	/>
 
 	<!-- Open Graph meta data -->
 	<meta property="og:url" content="https://robindimasin.com/" />
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content="Robin Dimasin" />
+	<meta property="og:title" content="Robin Dimasin | Freelance Python & Fullstack Developer" />
 	<meta
 		property="og:description"
-		content="Robin Dimasin is a Freelancer specializing in Python and Fullstack Development, offering services like Web Scraping, Browser Automation, and Task Automation. With experience as a System Analyst."
+		content="Robin Dimasin is a Freelancer specializing in Python and Fullstack Development, offering services like Web Scraping, Fullstack Development, Browser Automation using Selenium/Puppeteer, Python Scripting, Task Automation, and Chrome Extension Development. With experience as a System Analyst."
 	/>
 	<meta property="og:image" content="https://robindimasin.com/images/thumbnail.png" />
 
@@ -26,12 +26,16 @@
 	<meta name="twitter:title" content="Robin Dimasin" />
 	<meta
 		name="twitter:description"
-		content="Robin Dimasin is a Freelancer specializing in Python and Fullstack Development, offering services like Web Scraping, Browser Automation, and Task Automation. With experience as a System Analyst."
+		content="Robin Dimasin is a Freelancer specializing in Python and Fullstack Development, offering services like Web Scraping, Fullstack Development, Browser Automation using Selenium/Puppeteer, Python Scripting, Task Automation, and Chrome Extension Development. With experience as a System Analyst."
 	/>
 	<meta name="twitter:image" content="https://robindimasin.com/images/thumbnail.png" />
-	<meta data-rh="true" name="twitter:image:alt" content="Robin Dimasin" />
-	<meta data-rh="true" name="twitter:image:width" content="1138" />
-	<meta data-rh="true" name="twitter:image:height" content="576" />
+	<meta
+		data-rh="true"
+		name="twitter:image:alt"
+		content="Robin Dimasin | Freelance Python & Fullstack Developer"
+	/>
+	<meta data-rh="true" name="twitter:image:width" content="854" />
+	<meta data-rh="true" name="twitter:image:height" content="480" />
 	<meta name="twitter:creator" content="@RobinDimasin" />
 </svelte:head>
 
